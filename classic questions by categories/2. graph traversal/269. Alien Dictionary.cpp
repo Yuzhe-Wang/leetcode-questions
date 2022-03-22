3585e9a4346ae68aaa069bd0143e29189ec4989b
Yuzhe Wang <yuzhew@usc.edu>
2022-03-22 00:41:28 -0700

    Update 269. Alien Dictionary.cpp

--- classic questions by categories/2. graph traversal/269. Alien Dictionary.cpp
+++ classic questions by categories/2. graph traversal/269. Alien Dictionary.cpp
@@ -1,17 +1,21 @@
+/*
+Description:
+There is a new alien language that uses the English alphabet. However, the order among the letters is unknown to you.
+
+You are given a list of strings words from the alien language's dictionary, where the strings in words are sorted lexicographically by the rules of this new language.
+
+Return a string of the unique letters in the new alien language sorted in lexicographically increasing order by the new language's rules. If there is no solution, return "". If there are multiple solutions, return any of them.
+
+A string s is lexicographically smaller than a string t if at the first letter where they differ, the letter in s comes before the letter in t in the alien language. If the first min(s.length, t.length) letters are the same, then s is smaller if and only if s.length < t.length.
+*/
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <set>
-#include <map>
-#include <future>
-#include <functional>
-#include <utility>
 #include <unordered_map>
+#include <set>
 #include <unordered_set>
-#include <queue>
 
 using namespace std;
-
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
@@ -22,16 +26,16 @@ public:
         // store all letters
         unordered_set<char> letters;
         for(const auto& i: words) {
-            for(const auto& c: i) {
+            for(const auto c: i) {
                 letters.insert(c);
             }
         }
         
         // parse the input and create a graph(adjacency list)
         for(int i = 0; i < words.size(); ++i) {
-            for(int j = i + 1; j < words.size(); ++j) {
-                const string& a = words[i];
-                const string& b = words[j];
+            for(int j = i; j < words.size(); ++j) {
+                const string a = words[i];
+                const string b = words[j];
                 int length = min(a.length(), b.length());
                 int k = 0;
                 for(k = 0; k < length; ++k) {
@@ -44,9 +48,7 @@ public:
                         break;
                     }
                 }
-                if(a.length() > b.length() && k == length) {
-                    return "";
-                }
+                if(a.length() > b.length() && k == length) return "";
             }
         }
         
@@ -71,22 +73,23 @@ public:
     }
 private:
     bool dfs(char curr, unordered_map<char, vector<char>>& adj, string& ans,unordered_map<char, int>& states) { // return true if cycle detected
-        if(states[curr] == 1) return true; // cycle detected
-        if(states[curr] == 2) return false;
+        if(states[curr] == 1) {
+            return true; // cycle detected
+        }
+        if(states[curr] == 2) {
+            return false;
+        }
         
         states[curr] = 1;
-        for(const auto& i: adj[curr]) {
-            if(dfs(i, adj, ans, states)) return true;
+        if(adj.find(curr) != adj.end()) { //一定要检查，要不unordered_map会default construct,在线过不了
+            for(const auto& i: adj[curr]) {
+                if(dfs(i, adj, ans, states)) {
+                    return true;
+                }
+            }
         }
-        ans += curr;
         states[curr] = 2;
+        ans += curr;
         return false;
     }
 };
-
-int main() {
-    vector<string> input = {"dvpzu","bq","lwp","akiljwjdu","vnkauhh","ogjgdsfk","tnkmxnj","uvwa","zfe","dvgghw","yeyruhev","xymbbvo","m","n"};
-    Solution s;
-    string ans = s.alienOrder(input);
-    cout<<ans<<endl;
-}
